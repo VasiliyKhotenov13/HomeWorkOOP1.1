c8cc830886639889f6158714ad92224b3a8f06da
Пользователь <woodoo08@mail.ru>
2022-11-08 22:41:37 +0300

    Готовое ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car car1 = new Car("Lada", "Granta", 1.7f, "желтый", 2015, "Россия");
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0f, "чёрный", 2020, "Германия");
+        Car car3 = new Car("BMW", "Z8", 3.0f, "чёрный", 2021, "Германия");
+        Car car4 = new Car("KIA", "Sportage 4-го поколения", 2.4f, "красный", 2018, "Южная Корея");
+        Car car5 = new Car("Hyundai", "Avante", 1.6f, "оранжевый", 2016, "Южная корея");
+
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        System.out.println(car3.toString());
+        System.out.println(car4.toString());
+        System.out.println(car5.toString());
+
     }
 }
